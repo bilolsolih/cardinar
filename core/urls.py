478ba5d71bc92f7e80e65b21083f2d6b9652b618
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-08-11 18:40:12 +0500

    a lot of changes

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls.i18n import i18n_patterns
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
