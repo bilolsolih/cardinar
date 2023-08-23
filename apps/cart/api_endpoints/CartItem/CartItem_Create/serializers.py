fac0d912ab02b3270504c2fa39333265f7f5ab92
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-08-23 16:58:34 +0500

    Vahokazo hatikiri matikiri

--- apps/cart/api_endpoints/CartItem/CartItem_Create/serializers.py
+++ apps/cart/api_endpoints/CartItem/CartItem_Create/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-
+from django.db import transaction
 from apps.cart.models import CartItem
 
 
@@ -9,21 +9,30 @@ class CartItemCreateSerializer(serializers.ModelSerializer):
         fields = ['device_id', 'product', 'quantity', 'car_model']
 
     def validate(self, data):
-        if hasattr(data, 'device_id') and data['device_id'] and self.context['request'].user.is_authenticated:
+        user = self.context['request'].user
+        if hasattr(data, 'device_id') and data['device_id'] and user.is_authenticated:
             raise serializers.ValidationError('Authenticated users don\'t have to provide device_id.')
+
+        if 'device_id' in data and data['device_id']:
+            if CartItem.objects.filter(device_id=data['device_id'], product=data['product']).exists():
+                raise serializers.ValidationError({'product': 'Item already in the cart.'})
+        if user.is_authenticated:
+            if CartItem.objects.filter(cart=user.cart, product=data['product']).exists():
+                raise serializers.ValidationError({'product': 'Item already in the cart.'})
         return data
 
     def create(self, data):
-        user = self.context['request'].user
-        if user.is_authenticated:
-            item = CartItem.objects.create(
-                cart=user.cart, product=data['product'], quantity=data['quantity'], car_model=data['car_model'],
-                cost=(data['product'].price * data['quantity'])
-            )
-        else:
-            item = CartItem.objects.create(
-                device_id=data['device_id'], product=data['product'], quantity=data['quantity'], car_model=data['car_model'],
-                cost=(data['product'].price * data['quantity'])
-            )
-        item.clean()
-        return item
+        with transaction.atomic():
+            user = self.context['request'].user
+            if user.is_authenticated:
+                item = CartItem.objects.create(
+                    cart=user.cart, product=data['product'], quantity=data['quantity'], car_model=data['car_model'],
+                    cost=(data['product'].price * data['quantity'])
+                )
+            else:
+                item = CartItem.objects.create(
+                    device_id=data['device_id'], product=data['product'], quantity=data['quantity'], car_model=data['car_model'],
+                    cost=(data['product'].price * data['quantity'])
+                )
+            item.clean()
+            return item
