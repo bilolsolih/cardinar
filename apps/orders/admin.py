18409d839987d0f72e25f09ac828541c81920d6d
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-08-31 14:58:03 +0500

    images are compressed to png format now

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -3,6 +3,16 @@ from modeltranslation.admin import TranslationAdmin
 
 from .models import Order, OrderItem, PaymentType
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
+
+class OrderItemInOrder(admin.TabularInline):
+    model = OrderItem
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'status', 'phone_number']
+    list_display_links = ['id', 'status', 'phone_number']
+    inlines = [OrderItemInOrder]
+
+
 admin.site.register(PaymentType, TranslationAdmin)
