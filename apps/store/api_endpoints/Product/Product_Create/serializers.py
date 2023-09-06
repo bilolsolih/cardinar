54f4ced1ed8e7ccce38dc051c9369029cff89125
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-06 15:15:24 +0500

    images are compressed to png format now

--- apps/store/api_endpoints/Product/Product_Create/serializers.py
+++ apps/store/api_endpoints/Product/Product_Create/serializers.py
@@ -6,5 +6,7 @@ from apps.store.models.product import Product
 class ProductCreateSerializer(ModelSerializer):
     class Meta:
         model = Product
-        fields = ['category', 'title', 'title_ru', 'title_uz', 'type', 'status', 'photo', 'description',
-                  'description_ru', 'description_ru', 'price', 'car_models', 'main_color', 'building_material']
+        fields = [
+            'category', 'title', 'title_ru', 'title_uz', 'type', 'status', 'photo', 'description',
+            'description_ru', 'description_ru', 'price', 'car_models', 'main_color', 'building_material'
+        ]
