766c07befde7a911f29493ea2ab5b64fa11fde99
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-09 11:29:55 +0500

    images are compressed to png format now

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,9 +1,7 @@
 import os
-from whitenoise import WhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings.production")
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
