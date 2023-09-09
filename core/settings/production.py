05ca2986123d1d7d44c6da8b3edc22a07cfea2ff
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-09 15:13:27 +0500

    images are compressed to png format now

--- core/settings/production.py
+++ core/settings/production.py
@@ -2,17 +2,23 @@ from .base import *  # noqa
 from os import getenv
 
 ALLOWED_HOSTS = ['bilolsolih.pythonanywhere.com', 'api.cardinar.uz']
-
 DATABASES = {
     "default": {
-        "ENGINE": getenv('DB_ENGINE'),
-        "NAME": getenv('DB_NAME'),
-        "USER": getenv('DB_USER'),
-        "PASSWORD": getenv('DB_PASSWORD'),
-        "HOST": getenv('DB_HOST'),
-        "PORT": getenv('DB_PORT')
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+        "ATOMIC": False,
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": getenv('DB_ENGINE'),
+#         "NAME": getenv('DB_NAME'),
+#         "USER": getenv('DB_USER'),
+#         "PASSWORD": getenv('DB_PASSWORD'),
+#         "HOST": getenv('DB_HOST'),
+#         "PORT": getenv('DB_PORT')
+#     }
+# }
 
 DEBUG = True
 
