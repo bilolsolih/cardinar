2adbe5415c20def2faef75098d1dd0bf8f05f0f7
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-09 11:07:07 +0500

    images are compressed to png format now

--- core/settings/base.py
+++ core/settings/base.py
@@ -138,7 +138,7 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
