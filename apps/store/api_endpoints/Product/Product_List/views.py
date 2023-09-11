ac09e245ce0104074f09e1125eb9faba2f54d671
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-11 15:16:04 +0500

    images are compressed to png format now

--- apps/store/api_endpoints/Product/Product_List/views.py
+++ apps/store/api_endpoints/Product/Product_List/views.py
@@ -2,7 +2,7 @@ import django_filters
 from django_filters.rest_framework import FilterSet, DjangoFilterBackend
 from rest_framework.generics import ListAPIView
 
-from apps.store.models.product import Product
+from apps.store.models.product import Product, Category
 from apps.store.models.product_parameters import CarBrand, CarModel
 from .serializers import ProductListSerializer
 
@@ -11,10 +11,11 @@ class ProductFilterSet(FilterSet):
     title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
     car_brands = django_filters.ModelMultipleChoiceFilter(field_name='car_brands__pk', queryset=CarBrand.objects.all(), to_field_name='pk')
     car_models = django_filters.ModelMultipleChoiceFilter(field_name='car_models__pk', queryset=CarModel.objects.all(), to_field_name='pk')
+    category = django_filters.ModelMultipleChoiceFilter(field_name='category', queryset=Category.objects.all(), to_field_name='pk')
 
     class Meta:
         model = Product
-        fields = ['category', 'main_color', 'status']
+        fields = ['main_color', 'status']
 
 
 class ProductListAPIView(ListAPIView):
