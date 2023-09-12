4001db0e9bfb6210e79d7a99e05d519941133502
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-12 14:55:32 +0500

    images are compressed to png format now

--- core/settings/base.py
+++ core/settings/base.py
@@ -76,12 +76,12 @@ CORS_ALLOWED_ORIGINS = [
     'http://cardinar.uz'
 ]
 
-CSRF_TRUSTED_ORIGINS = [
-    'https://cardinar.uz',
-    'http://cardinar.uz',
-    'http://localhost:8080',
-    'https://localhost:8080',
-]
+# CSRF_TRUSTED_ORIGINS = [
+#     'https://cardinar.uz',
+#     'http://cardinar.uz',
+#     'http://localhost:8080',
+#     'https://localhost:8080',
+# ]
 
 ROOT_URLCONF = "core.urls"
 
