ce05002201313720fbf32c536cfb1eaa8421a9a5
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-19 12:00:38 +0500

    images are compressed to png format now

--- apps/store/api_endpoints/CarBrand/CarBrand_List/views.py
+++ apps/store/api_endpoints/CarBrand/CarBrand_List/views.py
@@ -1,12 +1,22 @@
+from django_filters.rest_framework.backends import DjangoFilterBackend
+from django_filters.rest_framework.filterset import FilterSet
 from rest_framework.generics import ListAPIView
 
 from apps.store.models.product_parameters import CarBrand
 from .serializers import CarBrandListSerializer
 
 
+class CarBrandFilterSet(FilterSet):
+    class Meta:
+        model = CarBrand
+        fields = ['products__category']
+
+
 class CarBrandListAPIView(ListAPIView):
     serializer_class = CarBrandListSerializer
     queryset = CarBrand.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = CarBrandFilterSet
 
 
 __all__ = ['CarBrandListAPIView']
