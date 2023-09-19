5b1c8fd480a2f253a6479bce228af7cad2aad856
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-19 12:14:45 +0500

    images are compressed to png format now

--- apps/store/api_endpoints/CarBrand/CarBrand_List/views.py
+++ apps/store/api_endpoints/CarBrand/CarBrand_List/views.py
@@ -1,22 +1,29 @@
-from django_filters.rest_framework.backends import DjangoFilterBackend
-from django_filters.rest_framework.filterset import FilterSet
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.generics import ListAPIView
 
 from apps.store.models.product_parameters import CarBrand
 from .serializers import CarBrandListSerializer
 
 
-class CarBrandFilterSet(FilterSet):
-    class Meta:
-        model = CarBrand
-        fields = ['products__category']
-
-
 class CarBrandListAPIView(ListAPIView):
     serializer_class = CarBrandListSerializer
     queryset = CarBrand.objects.all()
-    filter_backends = [DjangoFilterBackend]
-    filterset_class = CarBrandFilterSet
+
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter('category_id', openapi.IN_QUERY, description='Category id', type=openapi.TYPE_STRING),
+        ]
+    )
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def get_queryset(self):
+        category_id = self.kwargs.get('category_id', None)
+        if category_id:
+            return CarBrand.objects.filter(products__category__pk=category_id)
+        else:
+            return CarBrand.objects.all()
 
 
 __all__ = ['CarBrandListAPIView']
