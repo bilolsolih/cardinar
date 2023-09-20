e7dc32bd188b1845f8a167b4845c2a3440c90c85
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-20 16:34:15 +0500

    images are compressed to png format now

--- core/settings/base.py
+++ core/settings/base.py
@@ -231,7 +231,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
 EMAIL_HOST_USER = os.getenv('SMTP_USERNAME')
 EMAIL_HOST_PASSWORD = os.getenv('SMTP_PASSWORD')
 
--- core/settings/production.py
+++ core/settings/production.py
@@ -9,16 +9,6 @@ DATABASES = {
         "ATOMIC": False,
     }
 }
-# DATABASES = {
-#     "default": {
-#         "ENGINE": getenv('DB_ENGINE'),
-#         "NAME": getenv('DB_NAME'),
-#         "USER": getenv('DB_USER'),
-#         "PASSWORD": getenv('DB_PASSWORD'),
-#         "HOST": getenv('DB_HOST'),
-#         "PORT": getenv('DB_PORT')
-#     }
-# }
 
 DEBUG = True
 
