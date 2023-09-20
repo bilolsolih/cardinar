93f783df5ba372c97982b73f74c700915644ca21
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-20 17:13:56 +0500

    images are compressed to png format now

--- core/settings/base.py
+++ core/settings/base.py
@@ -142,9 +142,9 @@ LANGUAGES = [
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = '/home/cardina1/api.cardinar.uz/cardinar/static/'
 MEDIA_URL = "media/"
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = '/home/cardina1/api.cardinar.uz/cardinar/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
