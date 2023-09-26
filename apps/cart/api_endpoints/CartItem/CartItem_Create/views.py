9fc208d0d64cd73f3bc80f784a69badf8fcc8475
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-26 19:09:31 +0500

    fix: duplicate serializers

--- apps/cart/api_endpoints/CartItem/CartItem_Create/views.py
+++ apps/cart/api_endpoints/CartItem/CartItem_Create/views.py
@@ -1,8 +1,10 @@
 from rest_framework.generics import CreateAPIView
-
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
 from .serializers import CartItemCreateSerializer
 
 
+@method_decorator(csrf_exempt, name='post')
 class CartItemCreateAPIView(CreateAPIView):
     serializer_class = CartItemCreateSerializer
 
