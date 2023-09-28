3b288512c1b2df792055b7b4fe593bca8124a1ac
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-28 15:34:33 +0500

    Vahokazo hatikiri matikiri

--- apps/constructor/api_endpoints/CustomProduct/CustomProduct_Create/views.py
+++ apps/constructor/api_endpoints/CustomProduct/CustomProduct_Create/views.py
@@ -1,9 +1,11 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework.parsers import MultiPartParser, FormParser
 
 from .serializer import CustomProductCreateSerializer
 
 
 class CustomProductCreateAPIView(CreateAPIView):
+    parser_classes = [MultiPartParser, FormParser]
     serializer_class = CustomProductCreateSerializer
 
     def perform_create(self, serializer):
