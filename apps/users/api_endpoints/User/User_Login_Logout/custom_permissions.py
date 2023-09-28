7b663f7338101c88ad73916ab8a0b0a47b742381
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-28 10:46:19 +0500

    Vahokazo hatikiri matikiri

--- apps/users/api_endpoints/User/User_Login_Logout/custom_permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-
-class IsNotAuthenticated(BasePermission):
-    def has_permission(self, request, view):
-        return request.user.is_anonymous
