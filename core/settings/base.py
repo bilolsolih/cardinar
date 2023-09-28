68939d22cda4c501103a8439b5356a5c01955c32
BilolSolih <BilolMuhammadSolih@gmail.com>
2023-09-28 11:40:16 +0500

    Vahokazo hatikiri matikiri

--- core/settings/base.py
+++ core/settings/base.py
@@ -73,7 +73,8 @@ CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
     'https://localhost:8080',
     'https://cardinar.uz',
-    'http://cardinar.uz'
+    'http://cardinar.uz',
+    'http://192.168.2.167:8080/'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
